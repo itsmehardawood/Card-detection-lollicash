df2aea24138054068d37094b0c8441708a9294bd
itsmehardawood <mehar.dawood.official@gmail.com>
2025-07-02 00:37:31 +0500

    Maximum limit reached

--- src/app/components/ControlPanel.js
+++ src/app/components/ControlPanel.js
@@ -71,11 +71,10 @@ const ControlPanel = ({
       <div className="bg-white rounded-lg shadow-lg p-4 sm:p-6">
         <div className="text-center">
           <div className="bg-red-50 border border-red-200 rounded-lg p-4 mb-4">
-            <h3 className="text-lg font-semibold text-red-800 mb-2">ou have reached the current maximum scanning trials.</h3>
-            <p className="text-red-700 mb-3">{errorMessage}</p>
-            <p className="text-red-700 mb-3 font-bold">Contact your Customer Service team.</p>
+            <h3 className="text-lg font-semibold text-red-800 mb-2">Oops! Currently, you have reached the maximum number of times you can scan. </h3>
+            <p className="text-red-700 mb-3 font-bold">Contact customer support for assistance </p>
             <p className='text-red-700  '>or</p>
-            <p className="text-red-700 mb-3"> Try scanning again</p>
+            <p className="text-red-700 mb-3"> retry the scanning process.</p>
           </div>
         </div>
       </div>
